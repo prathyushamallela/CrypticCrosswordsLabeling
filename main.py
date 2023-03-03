ef6e8c224c7a19437413c17a191b64d9312dfbc6
Prathyusha <prathyushamallela@10-197-149-38.sds.oregonstate.edu>
2023-03-03 10:10:57 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+import 
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print("starting rule engine")
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
