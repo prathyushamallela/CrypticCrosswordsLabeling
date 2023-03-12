1ef32b16c1f5d6bf74c0136b4357e69ca5482b1b
Shaun10020 <10020guava@gmail.com>
2023-03-12 16:33:10 -0700

    include adapter-transformer library

--- Models/T5model.py
+++ Models/T5model.py
@@ -1,1 +1,22 @@
 from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers.adapters import T5AdapterModel
+import torch
+
+class T5model(torch.nn.modules):
+
+    def __init__():
+        super.__init__()
+        tokenizer = T5Tokenizer.from_pretrained("t5-small")
+        model = T5ForConditionalGeneration.from_pretrained("t5-small")
+
+class BiLSTMClassifier(torch.nn.modules):
+
+    def __init__(self,input_dim, output_dim, hidden_dim = 512):
+        super.__init__()
+        self.bilstm = torch.nn.LSTM(input_dim,hidden_dim,bidirectional=True)
+        self.linear = torch.nn.Linear(hidden_dim,output_dim)
+
+    def forward(self,x):
+        output, (ht,ct) = self.bilstm(x)
+        output = self.linear(x)
+        return output
