b1e7e383fbce11a4c9b7f6e995740e0888cc896b
Shaun10020 <10020guava@gmail.com>
2023-03-15 23:45:45 -0700

    small update

--- Training/train_adapter.py
+++ Training/train_adapter.py
@@ -1,4 +1,8 @@
 from Models import T5modelWithAdapter
 from config.configuration import T5_type, clue_type_classes,adapter_arguments
+from transformers.adapters import AdapterTrainer,setup_adapter_training
 
 T5Adapter = T5modelWithAdapter(T5_type,clue_type_classes)
+
+setup_adapter_training(T5Adapter,adapter_arguments,clue_type_classes[0])
+trainer = AdapterTrainer(model = T5Adapter)
--- config/configuration.py
+++ config/configuration.py
@@ -1,6 +1,6 @@
 import pathlib as Path
-from transformers.adapters import PrefixTuningConfig 
-from transformers.adapters.training import AdapterArguments
+from transformers import TrainingArguments
+from transformers.adapters import PrefixTuningConfig, AdapterArguments 
 
 clue_type_classes = ["anagram","hidden-word","container","reversal","deletion","homophone","double-def","charade","unclassified"]
 
@@ -10,5 +10,9 @@ All_T5_types = ['t5-small','t5-base','t5-large','t5-3b','t5-11b']
 mock_input = ["translate english to mandarin: This is my data","This is a mock data, I am testing.","Another mock data sentence, also for testing","Another one for testing"]
 
 save_file_path = Path.Path().cwd() / 'Model_saveflies'
+
 adapter_config = PrefixTuningConfig(flat = False, prefix_length = 30)
+## Refer to https://docs.adapterhub.ml/classes/adapter_training.html#transformers.adapters.training.AdapterArguments
 adapter_arguments = AdapterArguments(train_adapter = True,adapter_config =adapter_config)
+## Refer to https://huggingface.co/docs/transformers/main_classes/trainer#transformers.TrainingArguments
+training_arguments = TrainingArguments(output_dir= save_file_path)
