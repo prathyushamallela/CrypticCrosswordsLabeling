f00e24d4d04b3f1a07ddb8135b78c0ef6b2c3609
Shaun10020 <10020guava@gmail.com>
2023-03-19 01:58:22 -0700

    update

--- DataLoader/load_DataLoader_classifier.py
+++ DataLoader/load_DataLoader_classifier.py
@@ -2,5 +2,8 @@ from torch.utils.data import  DataLoader
 from datasets import load_dataset
 from config.configuration import data_file_path, data_file_name, batch_size
 
-    
-train_dataloader = DataLoader(load_dataset("json",data_files = str(data_file_path/data_file_name) )['train'],batch_size= batch_size, shuffle= True)
+
+train_ds = load_dataset("json",data_files = str(data_file_path/data_file_name) , split = ['train[:80%]'])
+test_ds = load_dataset("json",data_files = str(data_file_path/data_file_name) , split = ['train[80%:]'])
+
+train_dataloader = DataLoader(train_ds,batch_size= batch_size, shuffle= True)
