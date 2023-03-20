7435cb33f7c0d2a4bef679fabab5f8da23e8217f
Shaun10020 <10020guava@gmail.com>
2023-03-19 18:38:19 -0700

    update

--- DataLoader/load_DataLoader_classifier.py
+++ DataLoader/load_DataLoader_classifier.py
@@ -3,8 +3,8 @@ from config.configuration import data_file_path, batch_size
 from torch.utils.data import  DataLoader
 from utils.preprocess_data import tokenize_for_classifier
 
-train_ds = load_dataset("json",data_files = str(data_file_path/"cryptonite-naive-split-train_1679259144.471105.jsonl") , split = 'train')
-val_ds = load_dataset("json",data_files = str(data_file_path/"cryptonite-naive-split-val_1679259748.114214.jsonl") , split = 'train')
+train_ds = load_dataset("json",data_files = str(data_file_path/"sample1679170166.779008.jsonl") , split = 'train[:80%]')
+val_ds = load_dataset("json",data_files = str(data_file_path/"sample1679170166.779008.jsonl") , split = 'train[80%:]')
 
 train_ds = train_ds.map(tokenize_for_classifier,batched= True)
 val_ds = val_ds.map(tokenize_for_classifier,batched= True)
